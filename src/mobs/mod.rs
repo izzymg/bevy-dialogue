71f9161679ee7703a4d860a52f37aa462f8cd0c9
izzymg <izzymhg@gmail.com>
2022-02-09 18:37:30 +1300

    Load 3d model

--- src/mobs/mod.rs
+++ src/mobs/mod.rs
@@ -1,18 +1,13 @@
 use crate::interact;
 use bevy::prelude::*;
-
-pub fn setup_spawn_mob(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+pub fn setup_spawn_mob(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
-        .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_xyz(0.0, 1.0, 0.0),
-
-            ..Default::default()
+        .spawn_bundle((
+            Transform::from_xyz(0.0, 1.0, 0.0),
+            GlobalTransform::identity(),
+        ))
+        .with_children(|parent| {
+            parent.spawn_scene(asset_server.load("models/cube.gltf#Scene0"));
         })
         .insert(interact::Interactable(Vec3::ONE * 1.0));
 }
